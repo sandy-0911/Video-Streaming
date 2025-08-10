48ea058794836ecbb68b8252ec5c3308444f42ed
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-08-10 16:13:19 +0530

    file upload utilities and middlewares added

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {// req ke andar file ka path milega, file ke andar file ka naam milega
+    cb(null, "./public/temp") // ye public folder gitkeep mai hai
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) //achi practice nahi hai ye, kya pata user same naam ke 10 files upload karde , but bohot thode samay ke liye hi local storage pe saved hoti hai files so it doesn't matter much
+  }
+})
+
+const upload = multer({ storage: storage })
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from 'cloudinary' // client file bhejega mukter ko which is a receptionist , it will store the file in local storage and then upload it to cloudinary. Cloudianry will demand a local path to upload the file 
+import fs from 'fs' // file system module to delete files from local storage
+
+    // Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+//method banao jisme parameter mai local file ka path doge , succesfully upload hone par link ko unlink kar denge locally toh wo delete ho jayega 
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+            //upload file on cloudinary
+            const response = 
+            
+            
+            
+            await cloudinary.uploader.upload(localFilePath,{
+                resource_type: "auto" 
+            })
+            //file has been uploaded successfully
+            console.log("file is uploaded on cloudinary",
+                response.url);
+                return response          
+    }
+    catch(error){
+         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload was unsuccessful  
+         return null
+        }
+    }
+export {uploadOnCloudinary}
