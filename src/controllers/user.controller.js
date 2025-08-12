ae40fba2ca3625c0ad473ef097c0083e86051ab0
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-08-12 19:22:11 +0530

    bugs fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,4 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-
-
 const registerUser = asyncHandler(async (req, res) => { res.status(200).json({
     message: "ok"
   })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,7 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
 const router = Router()
-
 router.route("/register").post(registerUser)
 // router.route("/login").post(login)
 router.get('/test', (req, res) => res.send('Test route works'));
-
-
-
 export default router;
