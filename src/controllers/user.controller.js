e1f059c4e36417fa7d2dfa427e3472335a920171
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-08-15 17:05:46 +0530

    minor changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async (req, res) => { 
-  console.log("User registration successful")
+  console.log("Correct route hit")
   res.status(200).json({
     
     message: "api hit successfully",
