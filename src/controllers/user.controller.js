ce63112ab4978965af4ed4c67258808c6fc6a65a
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-08-15 18:34:54 +0530

    Registering a User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,23 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async (req, res) => { 
-  console.log("Correct route hit")
-  res.status(200).json({
-    
-    message: "api hit successfully",
-  })
+  // steps for registering a user
+  // get user details from frontend (from model) (or postman)
+  // validation of user details entered != not emopty
+  // check if user already exists in the database (from email, username)
+  // avatar and cover images files uploaded or not
+  // if present then upload to cloudinary,avatar
+  // create user  object - create entry in DB
+  // remove passowrd and refresh token field from response
+  // check for user creation 
+  //return response
+
+  //data handling from frontend
+  const {fullName, email, username, password}= req.body
+  console.log("User details: ", email);
+
+
+
+
 } )
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"; 
+
+
 const router = Router()
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1 // max 1 file for avatar
+        },
+        {
+            name: "coverImage",
+            maxCount: 1 // max 1 file for cover
+        },
+        {}
+    ]),
+    registerUser
+)
 // router.route("/login").post(login)
 router.get('/test', (req, res) => res.send('Test route works'));
 export default router;
