2567f6ddce03c9d801679c6142ae7c40b0f45e99
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-09-02 17:57:00 +0530

    few api changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
 
-  const existedUser = User.findOne({
+  const existedUser = await  User.findOne({
     $or: [{ username }, { email}]
   })
 
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ dotenv.config({
 
 
 
+
 connectDB()
 .then( () => {
         app.listen(process.env.PORT || 8000, () => {
