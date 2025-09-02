65e4f6a9c48d59edd4d2ea97926623e4b5b1522e
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-09-02 19:48:36 +0530

    bug fixes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,14 @@
-import multer from 'multer'
+import multer from "multer";
 
+// Storage config
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {// req ke andar file ka path milega, file ke andar file ka naam milega
-    cb(null, "./public/temp") // ye public folder gitkeep mai hai
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp"); // temp folder
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname) //achi practice nahi hai ye, kya pata user same naam ke 10 files upload karde , but bohot thode samay ke liye hi local storage pe saved hoti hai files so it doesn't matter much
-  }
-})
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
-export const upload = multer({
-    storage,
-})
+// Export multer instance
+export const upload = multer({ storage });
